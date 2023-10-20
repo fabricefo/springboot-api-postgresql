ba43ca03d372b428a17a28afb37735af01ec5cb8
Fabrice Fourel <ffourel@gmail.com>
2023-10-20 15:55:52 +0200

    Init project

---
+++ src/main/java/com/fabricefo/springbootapijpa/SpringbootapijpaApplication.java
@@ -0,0 +1,13 @@
+package com.fabricefo.springbootapijpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootapijpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootapijpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabricefo/springbootapijpa/SpringbootapijpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabricefo.springbootapijpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootapijpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
